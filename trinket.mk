fbccf2c1a74a26a1727ab4dc139b7a4882ad853e
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    trinket-common: bt: Add btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162
    Change-Id: I36ca083bf333826ac8980541181aa52022671396

--- trinket.mk
+++ trinket.mk
@@ -52,7 +52,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0 \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot SPL
 BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
