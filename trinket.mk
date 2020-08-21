75df8e8559fe69ed129581bd46d10897adeea8f7
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    trinket-common: Add Tethering Config
    Tethering package is APEX and we need to create a RRO to overwrite
    the values for config_tether_wifi_regexs and config_tether_wifi_p2p_regexs.
    BUG: 163800715
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- trinket.mk
+++ trinket.mk
@@ -405,6 +405,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
+    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf \
